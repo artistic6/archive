ec1f65aecaf8c4650827e8b24b14ea550719e023
artistic3 <artisticsoftware3@gmail.com>
2024-02-15 05:09:04 +0000

    ...

---
+++ archive/0207/plabets3.php
@@ -0,0 +1,85 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Favorite'  =>  '6',
+		'favorites' => '6',
+		'Trio values(Fav: 6)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Favorite'  =>  '8',
+		'favorites' => '8',
+		'Trio values(Fav: 8)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Favorite'  =>  '7',
+		'favorites' => '7',
+		'Trio values(Fav: 7)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Favorite'  =>  '1',
+		'favorites' => '1',
+		'Trio values(Fav: 1)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Favorite'  =>  '2',
+		'favorites' => '2',
+		'Trio values(Fav: 2)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Favorite'  =>  '8',
+		'favorites' => '8',
+		'Trio values(Fav: 8)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Favorite'  =>  '3',
+		'favorites' => '3',
+		'Trio values(Fav: 3)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Favorite'  =>  '4',
+		'favorites' => '4',
+		'Trio values(Fav: 4)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'Favorite'  =>  '3',
+		'favorites' => '3',
+		'Trio values(Fav: 3)' =>  '3, 4, 11',
+		'unionF(count: 3)' =>  '3, 4, 11',
+	],
+];
---
+++ archive/0212/plabets3.php
@@ -0,0 +1,103 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'Favorite'  =>  '4',
+		'favorites' => '4',
+		'Trio values(Fav: 4)' =>  '5, 10, 3',
+		'unionF(count: 3)' =>  '5, 10, 3',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'Favorite'  =>  '13',
+		'favorites' => '13',
+		'Trio values(Fav: 13)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'Favorite'  =>  '6',
+		'favorites' => '6',
+		'Trio values(Fav: 6)' =>  '3, 2, 5',
+		'unionF(count: 3)' =>  '3, 2, 5',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'Favorite'  =>  '10',
+		'favorites' => '10',
+		'Trio values(Fav: 10)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'Favorite'  =>  '4',
+		'favorites' => '4',
+		'Trio values(Fav: 4)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'Favorite'  =>  '3',
+		'favorites' => '3',
+		'Trio values(Fav: 3)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'Favorite'  =>  '4',
+		'favorites' => '4',
+		'Trio values(Fav: 4)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'Favorite'  =>  '6',
+		'favorites' => '6',
+		'Trio values(Fav: 6)' =>  '1',
+		'unionF(count: 1)' =>  '1',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'Favorite'  =>  '1',
+		'favorites' => '1',
+		'Trio values(Fav: 1)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'Favorite'  =>  '5',
+		'favorites' => '5',
+		'Trio values(Fav: 5)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'Favorite'  =>  '13',
+		'favorites' => '13',
+		'Trio values(Fav: 13)' =>  '',
+		'unionF(count: 0)' =>  '',
+	],
+];
