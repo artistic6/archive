cfce9ff3894b2526e072f26ebb1ea7ebe60b5390
artistic software <artisticsoftware41@gmail.com>
2024-03-31 03:50:32 +0000

    ...

--- archive/0207/bets.php
+++ archive/0207/bets.php
@@ -6,10 +6,10 @@ return [
 		Race 1
 		*/
 		'favorites' => '6, 1, 7',
-		'win hist(fav 1)' => '1, 4, 5, 7, 8, 10, 11',
 		'inter(fav 1, fav 6)' => '1, 4, 7, 11',
-		'win hist(fav 6)' => '1, 2, 4, 6, 7, 9, 11, 12',
-		'union' => '1, 4, 7, 11',
+		'inter(fav 1, fav 7)' => '5, 7',
+		'inter(fav 6, fav 7)' => '7',
+		'union' => '1, 4, 5, 7, 11',
 		'win' => '1, 6, 7',
 	],
 	'2' => [
@@ -17,34 +17,33 @@ return [
 		Race 2
 		*/
 		'favorites' => '4, 8',
+		'inter(fav 4, fav 8)' => '3',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'favorites' => '7',
-		'win hist(fav 7)' => '1, 3, 5, 6, 7, 11, 12',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'favorites' => '3, 1, 11',
-		'win hist(fav 1)' => '1, 2, 3, 5, 6, 10, 11',
 		'inter(fav 1, fav 3)' => '3, 5, 6, 11',
-		'win hist(fav 3)' => '3, 4, 5, 6, 8, 11',
-		'union' => '3, 5, 6, 11',
-		'win' => '1, 3, 11',
+		'inter(fav 1, fav 11)' => '6, 10, 11',
+		'inter(fav 3, fav 11)' => '6, 11',
+		'union' => '3, 5, 6, 10, 11',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
 		'favorites' => '7, 1, 2',
-		'win hist(fav 1)' => '1, 2, 3, 4, 5, 7, 10, 12',
+		'inter(fav 1, fav 2)' => '1, 2, 7',
 		'inter(fav 1, fav 7)' => '1, 4, 5, 7, 12',
-		'win hist(fav 7)' => '1, 4, 5, 7, 8, 12',
-		'union' => '1, 4, 5, 7, 12',
+		'inter(fav 2, fav 7)' => '1, 7, 8',
+		'union' => '1, 2, 4, 5, 7, 8, 12',
 		'win' => '1, 2, 7',
 	],
 	'6' => [
@@ -52,33 +51,31 @@ return [
 		Race 6
 		*/
 		'favorites' => '11, 8',
+		'inter(fav 8, fav 11)' => '12',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'favorites' => '2, 3',
-		'win hist(fav 2)' => '1, 2, 3, 4, 5, 6, 7, 9',
+		'inter(fav 2, fav 3)' => '1, 2, 3, 9',
+		'union' => '1, 2, 3, 9',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
 		'favorites' => '11, 4',
-		'win hist(fav 4)' => '1, 2, 4, 6, 7, 8, 9, 12',
+		'inter(fav 4, fav 11)' => '4, 9',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'favorites' => '5, 1, 3',
-		'win hist(fav 1)' => '1, 5, 6, 7, 8, 10, 12',
 		'inter(fav 1, fav 3)' => '5, 8, 10',
 		'inter(fav 1, fav 5)' => '5, 8, 10, 12',
-		'win hist(fav 3)' => '2, 3, 5, 8, 9, 10, 11',
 		'inter(fav 3, fav 5)' => '3, 5, 8, 10, 11',
-		'win hist(fav 5)' => '3, 4, 5, 8, 10, 11, 12',
-		'union' => '5, 8, 10, 12, 3, 11',
-		'win' => '1, 3, 5',
+		'union' => '3, 5, 8, 10, 11, 12',
 	],
 ];
--- archive/0212/bets.php
+++ archive/0212/bets.php
@@ -6,26 +6,27 @@ return [
 		Race 1
 		*/
 		'favorites' => '12, 4',
+		'inter(fav 4, fav 12)' => '10',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'favorites' => '12, 13, 2, 3',
-		'win hist(fav 2)' => '2, 4, 5, 6, 7, 8, 9, 12, 13',
 		'inter(fav 2, fav 3)' => '2, 4, 5, 6, 7, 8, 12, 13',
-		'win hist(fav 3)' => '2, 4, 5, 6, 7, 8, 10, 11, 12, 13',
-		'union' => '2, 4, 5, 6, 7, 8, 12, 13',
-		'win' => '2, 3, 12, 13',
+		'inter(fav 2, fav 12)' => '6, 13',
+		'inter(fav 2, fav 13)' => '13',
+		'inter(fav 3, fav 12)' => '6, 10, 13',
+		'inter(fav 3, fav 13)' => '13',
+		'inter(fav 12, fav 13)' => '13',
+		'union' => '2, 4, 5, 6, 7, 8, 10, 12, 13',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'favorites' => '6, 3',
-		'win hist(fav 3)' => '1, 3, 4, 7, 8, 9, 10, 12',
 		'inter(fav 3, fav 6)' => '1, 3, 4, 12',
-		'win hist(fav 6)' => '1, 3, 4, 5, 6, 12',
 		'union' => '1, 3, 4, 12',
 	],
 	'4' => [
@@ -33,27 +34,27 @@ return [
 		Race 4
 		*/
 		'favorites' => '2, 10, 1',
-		'win hist(fav 1)' => '1, 2, 3, 5, 6, 10, 11',
 		'inter(fav 1, fav 2)' => '2, 3, 5, 10, 11',
-		'win hist(fav 2)' => '2, 3, 5, 7, 9, 10, 11, 12, 13',
-		'union' => '2, 3, 5, 10, 11',
-		'win' => '1, 2, 10',
+		'inter(fav 1, fav 10)' => '10',
+		'inter(fav 2, fav 10)' => '10, 12',
+		'union' => '2, 3, 5, 10, 11, 12',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
 		'favorites' => '12, 4, 10',
-		'win hist(fav 4)' => '1, 2, 3, 4, 5, 6, 7',
+		'inter(fav 4, fav 10)' => '1, 2, 3, 4',
+		'inter(fav 4, fav 12)' => '2, 5, 6',
+		'inter(fav 10, fav 12)' => '2',
+		'union' => '1, 2, 3, 4, 5, 6',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'favorites' => '7, 3',
-		'win hist(fav 3)' => '1, 2, 3, 5, 7, 9, 11',
 		'inter(fav 3, fav 7)' => '1, 3, 5, 7, 9, 11',
-		'win hist(fav 7)' => '1, 3, 5, 6, 7, 9, 11',
 		'union' => '1, 3, 5, 7, 9, 11',
 	],
 	'7' => [
@@ -61,7 +62,6 @@ return [
 		Race 7
 		*/
 		'favorites' => '4',
-		'win hist(fav 4)' => '1, 2, 3, 4, 5, 7, 8, 10, 11',
 	],
 	'8' => [
 		/**
@@ -74,18 +74,29 @@ return [
 		Race 9
 		*/
 		'favorites' => '8, 12, 1',
-		'win hist(fav 1)' => '1, 5, 6, 7, 8, 10, 12',
+		'inter(fav 1, fav 8)' => '1, 5, 8',
+		'inter(fav 1, fav 12)' => '5, 12',
+		'inter(fav 8, fav 12)' => '5',
+		'union' => '1, 5, 8, 12',
+		'win' => '1, 8, 12',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
 		'favorites' => '13, 5',
+		'inter(fav 5, fav 13)' => '3',
 	],
 	'11' => [
 		/**
 		Race 11
 		*/
 		'favorites' => '4, 5, 6, 13',
+		'inter(fav 4, fav 5)' => '12',
+		'inter(fav 4, fav 6)' => '12',
+		'inter(fav 4, fav 13)' => '12',
+		'inter(fav 5, fav 6)' => '12',
+		'inter(fav 5, fav 13)' => '12',
+		'inter(fav 6, fav 13)' => '12',
 	],
 ];
