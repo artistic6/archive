ae2b0fe70f24c3bb767002804f9ee528d5eb69de
artistic software <artisticsoftware41@gmail.com>
2024-03-31 03:48:09 +0000

    ...

--- 0331/bets.php
+++ 0331/bets.php
@@ -6,16 +6,13 @@ return [
 		Race 1
 		*/
 		'favorites' => '8, 14',
-		'win hist(fav 8)' => '1, 2, 3, 5, 10, 12',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'favorites' => '2, 6',
-		'win hist(fav 2)' => '2, 4, 5, 6, 7, 8, 9, 12, 13',
 		'inter(fav 2, fav 6)' => '2, 4, 5, 6, 8',
-		'win hist(fav 6)' => '1, 2, 4, 5, 6, 8, 14',
 		'union' => '2, 4, 5, 6, 8',
 	],
 	'3' => [
@@ -35,21 +32,19 @@ return [
 		Race 5
 		*/
 		'favorites' => '3, 2',
-		'win hist(fav 3)' => '1, 3, 4, 5, 7, 10, 12',
+		'inter(fav 2, fav 3)' => '1, 7',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'favorites' => '3',
-		'win hist(fav 3)' => '1, 2, 3, 5, 7, 9, 11',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'favorites' => '4',
-		'win hist(fav 4)' => '1, 2, 3, 4, 5, 7, 8, 10, 11',
 	],
 	'8' => [
 		/**
@@ -57,23 +52,23 @@ return [
 		*/
 		'favorites' => '1, 14, 4',
 		'additional favorites' => '10, 13',
-		'win hist(fav 4)' => '1, 2, 4, 6, 7, 8, 9, 12',
+		'inter(fav 1, fav 4)' => '1, 6, 7',
+		'inter(fav 1, fav 10)' => '7',
 		'inter(fav 4, fav 10)' => '2, 4, 7, 9',
-		'win hist(fav 10)' => '2, 3, 4, 7, 9, 10',
-		'union' => '2, 4, 7, 9',
-		'win' => '1, 4, 10, 13, 14',
+		'union' => '1, 2, 4, 6, 7, 9',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'favorites' => '13, 8, 10',
-		'win hist(fav 10)' => '1, 3, 5, 6, 10, 11',
+		'inter(fav 8, fav 10)' => '1, 5',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
 		'favorites' => '8, 7, 1',
+		'inter(fav 7, fav 8)' => '7, 8',
 	],
 ];
