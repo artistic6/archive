1344491bddca45ae3da6ad10ba50f79f7ce16087
artistic software <artisticsoftware41@gmail.com>
2024-04-06 08:51:29 +0000

    ...

---
+++ 0407/bets.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '12',
+		'win hist(fav 12)' => '10, 12',//count: 2
+		'all' => '10, 12',//count: 2
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '10',
+		'win hist(fav 10)' => '10',//count: 1
+		'all' => '10',//count: 1
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '2',
+		'win hist(fav 2)' => '1, 2, 5, 8',//count: 4
+		'all' => '1, 2, 5, 8',//count: 4
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '8',
+		'win hist(fav 8)' => '1, 3, 8, 11',//count: 4
+		'all' => '1, 3, 8, 11',//count: 4
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '6',
+		'win hist(fav 6)' => '3, 4, 6, 7, 9',//count: 5
+		'all' => '3, 4, 6, 7, 9',//count: 5
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '1',
+		'win hist(fav 1)' => '1, 3, 7, 9, 12, 13',//count: 6
+		'all' => '1, 3, 7, 9, 12, 13',//count: 6
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '3',
+		'win hist(fav 3)' => '1, 2, 3, 9, 10',//count: 5
+		'all' => '1, 2, 3, 9, 10',//count: 5
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '2',
+		'win hist(fav 2)' => '2, 4, 5, 6, 9, 11',//count: 6
+		'all' => '2, 4, 5, 6, 9, 11',//count: 6
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '4',
+		'win hist(fav 4)' => '4, 5, 6, 7, 9',//count: 5
+		'all' => '4, 5, 6, 7, 9',//count: 5
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '9',
+		'win hist(fav 9)' => '2, 11',//count: 2
+		'all' => '2, 9, 11',//count: 3
+	],
+];
---
+++ 0407/sorted.php
@@ -0,0 +1,105 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '3, 7, 12',
+		'inter(fav 3, fav 7)' => '3, 5, 7',
+		'inter(fav 3, fav 12)' => '10, 12',
+		'union' => '3, 5, 7, 10, 12',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '3, 10, 11',
+		'inter(fav 3, fav 10)' => '10',
+		'inter(fav 3, fav 11)' => '11',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '2, 4, 9',
+		'inter(fav 2, fav 4)' => '2, 5, 8',
+		'inter(fav 4, fav 9)' => '3',
+		'union' => '2, 3, 5, 8',
+		'win' => '2, 4, 9',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '2, 3, 8',
+		'inter(fav 2, fav 3)' => '3, 5, 11',
+		'inter(fav 2, fav 8)' => '1, 3, 11',
+		'inter(fav 3, fav 8)' => '3, 8, 11',
+		'union' => '1, 3, 5, 8, 11',
+		'win' => '2, 3, 8',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '3, 6, 8',
+		'inter(fav 3, fav 6)' => '3, 4, 7',
+		'inter(fav 3, fav 8)' => '1, 4, 5, 7',
+		'inter(fav 6, fav 8)' => '4, 7, 9',
+		'union' => '1, 3, 4, 5, 7, 9',
+		'win' => '3, 6, 8',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '1, 2, 4',
+		'inter(fav 1, fav 2)' => '3',
+		'inter(fav 1, fav 4)' => '3, 7, 12, 13',
+		'inter(fav 2, fav 4)' => '3, 8',
+		'union' => '3, 7, 8, 12, 13',
+		'win' => '1, 2, 4',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '1, 2, 3',
+		'inter(fav 1, fav 2)' => '1, 2, 4, 5, 7, 9',
+		'inter(fav 1, fav 3)' => '1, 2, 9',
+		'inter(fav 2, fav 3)' => '1, 2, 3, 9',
+		'union' => '1, 2, 3, 4, 5, 7, 9',
+		'win' => '1, 2, 3',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '2, 3, 7',
+		'inter(fav 2, fav 3)' => '5, 6',
+		'inter(fav 2, fav 7)' => '2, 4, 5',
+		'inter(fav 3, fav 7)' => '1, 5, 12',
+		'union' => '1, 2, 4, 5, 6, 12',
+		'win' => '2, 3, 7',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '4, 7, 11',
+		'inter(fav 4, fav 7)' => '5, 6, 9',
+		'inter(fav 4, fav 11)' => '5, 9',
+		'inter(fav 7, fav 11)' => '5, 9, 12',
+		'union' => '5, 6, 9, 12',
+		'win' => '4, 7, 11',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '1, 3, 9',
+		'inter(fav 1, fav 3)' => '4',
+		'inter(fav 3, fav 9)' => '11',
+	],
+];
