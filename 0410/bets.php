2e5d3b35c82ab101c6fb85bb72b0b142e1f0cd06
artistic software <artisticsoftware41@gmail.com>
2024-04-09 06:40:20 +0000

    ...

---
+++ 0410/bets.php
@@ -0,0 +1,85 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '5',
+		'win hist' => '1, 5, 7, 8, 9',//count: 5
+		'all' => '1, 5, 7, 8, 9',//count: 5
+		'place' => '7',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '3',
+		'win hist' => '2, 4, 5, 6, 7, 8, 10, 11, 12',//count: 9
+		'all' => '2, 3, 4, 5, 6, 7, 8, 10, 11, 12',//count: 10
+		'place' => '2',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '3',
+		'win hist' => '1, 3, 4, 7, 8',//count: 5
+		'all' => '1, 3, 4, 7, 8',//count: 5
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1',
+		'win hist' => '1, 2, 3, 5, 6, 10, 11',//count: 7
+		'all' => '1, 2, 3, 5, 6, 10, 11',//count: 7
+		'place' => '2',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '5',
+		'win hist' => '1, 3, 4, 5, 12',//count: 5
+		'all' => '1, 3, 4, 5, 12',//count: 5
+		'place' => '5',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '6',
+		'win hist' => '1, 2, 4, 6, 7, 10, 11, 12',//count: 8
+		'all' => '1, 2, 4, 6, 7, 10, 11, 12',//count: 8
+		'place' => '7',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '10',
+		'win hist' => '3, 8, 10, 12',//count: 4
+		'all' => '3, 8, 10, 12',//count: 4
+		'place' => '3',
+		'place' => '8',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '3',
+		'win hist' => '1, 3, 5, 6, 7, 8, 12',//count: 7
+		'all' => '1, 3, 5, 6, 7, 8, 12',//count: 7
+		'place' => '7',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '9',
+		'win hist' => '12',//count: 1
+		'all' => '9, 12',//count: 2
+		'place' => '12',
+	],
+];
