d335aa9b311ed146e62bed3f29e03b05b30f2981
artistic software <artisticsoftware41@gmail.com>
2024-04-11 03:17:18 +0000

    ...

--- archive/0407/bets.php
+++ archive/0407/bets.php
@@ -6,14 +6,14 @@ return [
 		Race 1
 		*/
 		'favorites' => '7, 12',
-		'win hist' => '7, 8, 10, 12',//count: 4
+		'win hist' => '2, 3, 5, 7, 8, 10, 12',//count: 7
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'favorites' => '4, 5, 10',
-		'win hist' => '10',//count: 1
+		'win hist' => '3, 4, 5, 7, 10, 12',//count: 6
 		'place' => '10',
 	],
 	'3' => [
@@ -28,7 +28,7 @@ return [
 		Race 4
 		*/
 		'favorites' => '4, 8',
-		'win hist' => '1, 2, 3, 8, 11',//count: 5
+		'win hist' => '1, 2, 3, 4, 8, 9, 11, 12',//count: 8
 		'place' => '8',
 	],
 	'5' => [
@@ -36,14 +36,14 @@ return [
 		Race 5
 		*/
 		'favorites' => '3, 6',
-		'win hist' => '1, 3, 4, 6, 7, 9',//count: 6
+		'win hist' => '1, 3, 4, 5, 6, 7, 9',//count: 7
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'favorites' => '1, 2, 4',
-		'win hist' => '3, 4, 6, 7, 8, 12, 13',//count: 7
+		'win hist' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13',//count: 12
 		'place' => '4',
 	],
 	'7' => [
@@ -51,27 +51,27 @@ return [
 		Race 7
 		*/
 		'favorites' => '1, 3',
-		'win hist' => '1, 2, 3, 9, 10',//count: 5
+		'win hist' => '1, 2, 3, 4, 5, 7, 8, 9, 10',//count: 9
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
 		'favorites' => '2, 3, 7',
-		'win hist' => '1, 2, 3, 4, 5, 8, 12',//count: 7
+		'win hist' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12',//count: 11
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'favorites' => '4, 7',
-		'win hist' => '5, 6, 7, 9, 12',//count: 5
+		'win hist' => '4, 5, 6, 7, 9, 12',//count: 6
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
 		'favorites' => '1, 3, 9',
-		'win hist' => '2, 10, 11',//count: 3
+		'win hist' => '1, 2, 3, 4, 6, 7, 10, 11',//count: 8
 	],
 ];
