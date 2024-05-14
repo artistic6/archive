a418d58715b229306d229472151260d1789d1ea0
artistic software <artisticsoftware41@gmail.com>
2024-05-14 06:20:14 +0000

    ...

--- 0515/bets.php
+++ 0515/bets.php
@@ -5,15 +5,20 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '1, 10',
-		'union' => '1, 4, 5, 7, 8, 10, 11',//count: 7
-		'inter' => '1, 10',
+		'favorites' => '1, 8, 10',
+		'win($20)' => '1, 8, 10',
+		'win($20)' => '8, 10',
+		'qin/trio($10)' => '1, 8, 10',
+		'union' => '1, 3, 4, 5, 7, 8, 10, 11',//count: 8
+		'inter' => '1, 8, 10',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'favorites' => '4',
+		'favorites' => '4, 9',
+		'union' => '3, 4, 8',//count: 3
+		'inter' => '4',
 	],
 	'3' => [
 		/**
@@ -37,9 +42,9 @@ return [
 		/**
 		Race 6
 		*/
-		'favorites' => '4, 6, 9',
-		'union' => '1, 2, 4, 6, 7, 9',//count: 6
-		'inter' => '4, 6, 9',
+		'favorites' => '4, 6, 9, 10',
+		'union' => '1, 2, 3, 4, 6, 7, 9, 10, 12',//count: 9
+		'inter' => '4, 6, 9, 10',
 	],
 	'7' => [
 		/**
@@ -53,7 +58,9 @@ return [
 		/**
 		Race 8
 		*/
-		'favorites' => '3',
+		'favorites' => '3, 4',
+		'union' => '3, 6, 7, 8, 12',//count: 5
+		'inter' => '3',
 	],
 	'9' => [
 		/**
