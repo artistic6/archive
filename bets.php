20dc5f1090980af28264222e726f405e09218846
artistic software <artisticsoftware41@gmail.com>
2024-05-16 08:12:42 +0000

    ...

--- bets.php
+++ bets.php
@@ -6,7 +6,8 @@ $step = "bets";
 $raceDate = trim($argv[1]);
 $currentDir = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
 
-$allRacesOdds = include($currentDir . DIRECTORY_SEPARATOR . "odds.php");
+$oddsFile = $currentDir . DIRECTORY_SEPARATOR . "odds.php";
+if(file_exists($oddsFile)) $allRacesOdds = include($oddsFile);
 $history = include(__DIR__ . DIRECTORY_SEPARATOR . "winhistory.php");
 $outFile = $currentDir . DIRECTORY_SEPARATOR . "$step.php";
 
@@ -14,23 +15,28 @@ if(file_exists($outFile)){
     $oldData = include($outFile);
 }
 
-$totalRaces = count($allRacesOdds);
+if(file_exists($oddsFile)) $totalRaces = count($allRacesOdds);
+elseif(file_exists($outFile)) $totalRaces = count($oldData);
+else die('No input Files!');
 
 $outtext = "<?php\n\n";
 $outtext .= "return [\n";
+
 for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
-    if(!isset($allRacesOdds[$raceNumber])) continue;
     if(isset($oldData)){
         if(isset($oldData[$raceNumber]['favorites'])) $oldFavorites = explode(", ", $oldData[$raceNumber]['favorites']);
         if(isset($oldData[$raceNumber]['official win'])) $officialWin = explode(", ", $oldData[$raceNumber]['official win']);
     }
     if(isset($oldFavorites)) $favorites = $oldFavorites;
     else $favorites = [];
-    $winsArray = $allRacesOdds[$raceNumber];
-    asort($winsArray);
-    $runners = array_keys($winsArray);
-    $favorite = $runners[0];
-    if(!in_array($favorite, $favorites)) $favorites[] = $favorite;
+    if(isset($allRacesOdds)){
+        $winsArray = $allRacesOdds[$raceNumber];
+        asort($winsArray);
+        $runners = array_keys($winsArray);
+        $favorite = $runners[0];
+        if(!in_array($favorite, $favorites)) $favorites[] = $favorite;
+    }
+    else $runners = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
     sort($favorites);
     $racetext = "";
     $racetext .= "\t'$raceNumber' => [\n";
@@ -56,6 +62,25 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
         $racetext .= "\t\t'win($20)' => '" . implode(", ", $favorites) . "',\n"; 
         $racetext .= "\t\t'win($20)' => '" . implode(", ", array_slice($favorites, 1, 2)) . "',\n"; 
         $racetext .= "\t\t'qin/trio($10)' => '" . implode(", ", $favorites) . "',\n"; 
+    }
+    elseif(count($favorites) > 3){
+        foreach($favorites as $one){
+            $win1 = array_intersect($history[$raceNumber][$one]["win"], $runners);
+            foreach($favorites as $two){
+                if($two > $one){
+                    $win2 = array_intersect($history[$raceNumber][$two]["win"], $runners);
+                    foreach($favorites as $three){
+                        if($three > $two){
+                            $win3 = array_intersect($history[$raceNumber][$three]["win"], $runners);
+                            $intersection = array_intersect($win1, $win2, $win3, [$one, $two, $three]);
+                            if(count($intersection) >= 2){
+                                $racetext .= "\t\t'possible win' => '" . implode(", ", [$one, $two, $three]) . "',\n"; 
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     $union = [];
     foreach($favorites as $one){
