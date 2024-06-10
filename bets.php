2801a26757ec4f51eb7fac73e2c6290ca1261f4c
artistic software <artisticsoftware41@gmail.com>
2024-06-10 02:45:36 +0000

    ...

--- bets.php
+++ bets.php
@@ -69,7 +69,9 @@ for ($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber++) {
     $racetext .= "\t\tRace $raceNumber\n";
     $racetext .= "\t\t*/\n";
     $racetext .= "\t\t'favorites' => '" . implode(", ", $favorites) . "',\n"; 
-   
+    sort($runners);
+    $racetext .= "\t\t'runners' => '" . implode(", ", $runners) . "',\n"; 
+    $nonFavorites = array_diff($runners, $favorites);
     if(isset($officialWin)){
         $racetext .= "\t\t'official win' => '" . implode(", ", $officialWin) . "',\n"; 
     }
@@ -129,11 +131,15 @@ for ($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber++) {
     $racetext .= "\t\t'trio pivots' => '" . implode(", ", $pivots) . "',\n";
     $unitBet = 100;
     $compactExpr = count($wininter) . count($qininter) . count($winpivots) . count($qinpivots) . count($pivots);
-    $favoriteWin = ['11134', '11245', '11255', '11355', '11444', '11555', '12223', '12233', '12334', '12344', '12355', '12445', '12555', '13244', '13333', '13355', '14444', '14566', '22233', '23233', '23333', '33333'];
-    $favoriteQin = ['11134', '11255', '12344', '12445', '33333'];
-    $favoriteTrio = ['11255', '12344', '12445'];
-    $biggestFavoriteWin = ['11134', '11355', '22233', '23333'];
-    $biggestFavoritePlace = ['11134', '11255', '11355', '11666', '12344', '22223', '22233', '22333', '22344', '23233', '23333', '33333'];
+    $favoriteWin = ['00012', '00023', '00033', '00111', '00133', '00134', '00144', '00223', '00334', '00444', '00555', '01022', '01223', '01234', '01245', '01255', '01355', '01444', '01455', '02223', '02233', '02344', '02445', '02555', '04566', '11133', '11444', '12223', '12233', '12334', '12344', '12355', '12555', '13244', '13333', '13355', '14444', '22233', '23233', '23333', '33333'];
+    $favoriteQin = ['00023', '00033', '00134', '00334', '01223', '01255', '01455', '02344', '02445', '02555', '11444', '12223', '12344', '33333'];
+    $favoriteTrio = ['00334', '01223', '01255', '02344', '02445', '12223', '12344'];
+    $nonFavoriteWin = ['00022', '00112', '00333', '00455', '00666', '01000', '01023', '01111', '01112', '01113', '01122', '01123', '01144', '01222', '02222', '02333', '02777', '03133', '03233', '11222', '11244', '11344', '12123', '12444', '13444', '22223', '22344'];
+    $nonFavoriteQin = ['00022', '01113', '01122', '01222', '12123', '12444', '22344'];
+    $nonFavoriteTrio = ['01113', '12123'];
+    $biggestFavoriteWin = ['00134', '01355', '02344', '11133', '12223', '12233', '22233', '23333'];
+    $biggestFavoritePlace = ['00002', '00134', '00334', '00444', '00666', '01022', '01122', '01123', '01144', '01223', '01255', '01355', '02222', '02344', '11133', '12223', '12233', '12344', '22223', '22233', '22333', '22344', '23233', '23333', '33333'];
+    
     
     if(in_array($compactExpr, $favoriteWin)){
         $racetext .= "\t\t'win($" . $unitBet . ")' => '" . implode(", ", $favorites) . "',\n"; 
@@ -159,6 +165,21 @@ for ($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber++) {
         $racetext .= "\t\t'place($" . 10 * $unitBet . ")' => '" .  end($favorites)  . "',\n"; 
         $totalBets[$raceNumber] += 10 * $unitBet;
         $totalPlace -= 10 * $unitBet;
+    }
+    if(in_array($compactExpr, $nonFavoriteWin)){
+        $racetext .= "\t\t'win($10)' => '" . implode(", ", $nonFavorites) . "',\n"; 
+        $totalBets[$raceNumber] += 10 * count($nonFavorites);
+        $totalWin -= 10 * count($nonFavorites);
+    }
+    if(in_array($compactExpr, $nonFavoriteQin)){
+        $racetext .= "\t\t'qin($10)' => '" . implode(", ", $nonFavorites) . "',\n"; 
+        $totalBets[$raceNumber] += 10 * combination(2, count($nonFavorites));
+        $totalQin -= 20 * combination(2, count($nonFavorites));
+    }
+    if(in_array($compactExpr, $nonFavoriteTrio)){
+        $racetext .= "\t\t'trio($10)' => '" . implode(", ", $nonFavorites) . "',\n"; 
+        $totalBets[$raceNumber] += 10 * combination(3, count($nonFavorites));
+        $totalTrio -= 10 * combination(3, count($nonFavorites));
     }
     if(isset($officialWin) && $totalBets[$raceNumber] > 0){
         $totalRace[$raceNumber] -= $totalBets[$raceNumber];
@@ -186,6 +207,21 @@ for ($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber++) {
         if(in_array($compactExpr, $favoriteTrio) && count(array_intersect($favorites, array_slice($officialWin, 0, 3))) === 3){
             $totalRace[$raceNumber] += $trioAmount;
             $racetext .= "\t\t'5 won(trio bet)' => " . $trioAmount . ",\n";
+            $totalTrio += $trioAmount;
+        }
+        if(in_array($compactExpr, $nonFavoriteWin) && in_array($officialWin[0], $nonFavorites)){
+            $totalRace[$raceNumber] += $winAmount;
+            $racetext .= "\t\t'1N won(win bet)' => " . $winAmount . ",\n";
+            $totalWin += $winAmount;
+        }
+        if(in_array($compactExpr, $nonFavoriteQin) && count(array_intersect($nonFavorites, array_slice($officialWin, 0, 2))) === 2){
+            $totalRace[$raceNumber] += $qinAmount;
+            $racetext .= "\t\t'2N won(qin bet)' => " . $qinAmount . ",\n";
+            $totalQin += $qinAmount;
+        }
+        if(in_array($compactExpr, $nonFavoriteTrio) && count(array_intersect($nonFavorites, array_slice($officialWin, 0, 3))) === 3){
+            $totalRace[$raceNumber] += $trioAmount;
+            $racetext .= "\t\t'5N won(trio bet)' => " . $trioAmount . ",\n";
             $totalTrio += $trioAmount;
         }
         $racetext .= "\t\t'total won in race' => " . $totalRace[$raceNumber] . ",\n";
