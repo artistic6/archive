50e2af9f8deb6f0020753c465d55698c92441e1b
artistic software <artisticsoftware41@gmail.com>
2024-06-11 04:27:50 +0000

    ...

---
+++ 0612/bets.php
@@ -0,0 +1,118 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '6',
+		'qin inter' => '6',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '4',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '4',
+		'qin inter' => '4',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '3',
+		'qin inter' => '3',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '8',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '',
+		'qin inter' => '8',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+		'win($10)' => '1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'win inter' => '3',
+		'qin inter' => '3',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '5',
+		'qin inter' => '5',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '11',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '',
+		'qin inter' => '',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'win inter' => '6',
+		'qin inter' => '6',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '3',
+		'qin inter' => '3',
+		'win pivots' => '',
+		'qin pivots' => '',
+		'trio pivots' => '',
+	],
+];
+//total win: -110
+//total place: 0
+//total qin: 0
+//total trio: 0
+//total: 0
