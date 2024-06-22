b8ca590d7777d9b19218bd20d9f4187763fab9fb
artistic software <artisticsoftware41@gmail.com>
2024-06-22 10:01:34 +0000

    ...

--- 0623/bets.php
+++ 0623/bets.php
@@ -55,9 +55,9 @@ return [
 		/**
 		Race 6
 		*/
-		'favorites' => '3, 6',
+		'favorites' => '1, 3, 6',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
-		'win inter' => '',
+		'win inter' => '1',
 		'count sets' => 7,
 		'allValues' => '1, 2, 3, 5, 6, 7, 8, 9, 10',
 	],
