ade6e3cf754545cee34487166984307af89b3e61
artistic software <artisticsoftware41@gmail.com>
2024-06-22 04:13:19 +0000

    ...

---
+++ 0623/bets.php
@@ -0,0 +1,119 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '14',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '',
+		'count sets' => 21,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
+		'win inter' => '3',
+		'count sets' => 10,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '7',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '7',
+		'count sets' => 4,
+		'allValues' => '1, 2, 3, 5, 6, 7',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '',
+		'count sets' => 11,
+		'allValues' => '1, 2, 3, 4, 5, 6, 8, 10, 11, 12',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '14',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '',
+		'count sets' => 2,
+		'allValues' => '1, 3, 4, 5, 10, 11',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '3',
+		'count sets' => 7,
+		'allValues' => '1, 2, 3, 5, 6, 7, 8, 9, 10',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '8',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '8',
+		'count sets' => 16,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7',
+		'win inter' => '5',
+		'count sets' => 7,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '11',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'win inter' => '',
+		'count sets' => 15,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '',
+		'count sets' => 0,
+		'allValues' => '',
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'favorites' => '2',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'win inter' => '',
+		'count sets' => 0,
+		'allValues' => '',
+	],
+];
+//total win: 0
+//total place: 0
+//total qin: 0
+//total trio: 0
+//total: 0
