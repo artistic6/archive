e9a85941b71bbb8c4d5b4c97093fffc2e80dc16a
artistic41 <95563532+artistic41@users.noreply.github.com>
2024-06-26 11:07:20 +0800

    Update bets.php

--- 0626/bets.php
+++ 0626/bets.php
@@ -5,7 +5,7 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '8',
+		'favorites' => '2, 5, 8',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
 		'win inter' => '8',
 		'count sets' => 21,
@@ -15,9 +15,9 @@ return [
 		/**
 		Race 2
 		*/
-		'favorites' => '5',
+		'favorites' => '5, 6, 10',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '5',
+		'win inter' => '10',
 		'count sets' => 10,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
 	],
@@ -25,9 +25,9 @@ return [
 		/**
 		Race 3
 		*/
-		'favorites' => '6',
+		'favorites' => '1, 6',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '',
+		'win inter' => '1',
 		'count sets' => 4,
 		'allValues' => '1, 2, 3, 5, 6, 7',
 		'win($10)' => '1, 2, 3, 5, 6, 7',
@@ -36,19 +36,19 @@ return [
 		/**
 		Race 4
 		*/
-		'favorites' => '2',
+		'favorites' => '2, 12',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '',
-		'count sets' => 15,
-		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12',
+		'win inter' => '12',
+		'count sets' => 11,
+		'allValues' => '1, 2, 3, 4, 5, 6, 8, 10, 11, 12',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'favorites' => '5',
+		'favorites' => '1, 2, 5',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '5',
+		'win inter' => '1',
 		'count sets' => 2,
 		'allValues' => '1, 3, 4, 5, 10, 11',
 		'win($10)' => '1, 3, 4, 5, 10, 11',
@@ -60,7 +60,7 @@ return [
 		'favorites' => '5',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'win inter' => '5',
-		'count sets' => 10,
+		'count sets' => 7,
 		'allValues' => '1, 2, 3, 5, 6, 7, 8, 9, 10',
 	],
 	'7' => [
@@ -70,31 +70,32 @@ return [
 		'favorites' => '3',
 		'runners' => '1, 3, 4, 5, 6, 7, 8, 9, 10',
 		'win inter' => '3',
-		'count sets' => 12,
-		'allValues' => '1, 3, 4, 5, 6, 7, 8, 9',
+		'count sets' => 6,
+		'allValues' => '1, 3, 4, 5, 6, 7',
+		'win($10)' => '1, 3, 4, 5, 6, 7',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'favorites' => '9',
+		'favorites' => '2, 3, 5, 9, 12',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '9',
-		'count sets' => 15,
+		'win inter' => '',
+		'count sets' => 12,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
-		'favorites' => '1',
+		'favorites' => '2, 9, 10',
 		'runners' => '1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '1',
+		'win inter' => '',
 		'count sets' => 11,
 		'allValues' => '1, 2, 3, 5, 6, 7, 8, 9, 10, 11',
 	],
 ];
-//total win: -120
+//total win: -180
 //total place: 0
 //total qin: 0
 //total trio: 0
