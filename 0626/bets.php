0dad837b7ae3dd785e076417acb7540db58b84d4
artistic software <artisticsoftware41@gmail.com>
2024-06-26 03:06:07 +0000

    ...

---
+++ 0626/bets.php
@@ -0,0 +1,101 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '8',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'win inter' => '8',
+		'count sets' => 21,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '5',
+		'count sets' => 10,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '',
+		'count sets' => 4,
+		'allValues' => '1, 2, 3, 5, 6, 7',
+		'win($10)' => '1, 2, 3, 5, 6, 7',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '2',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '',
+		'count sets' => 15,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '5',
+		'count sets' => 2,
+		'allValues' => '1, 3, 4, 5, 10, 11',
+		'win($10)' => '1, 3, 4, 5, 10, 11',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '5',
+		'count sets' => 10,
+		'allValues' => '1, 2, 3, 5, 6, 7, 8, 9, 10',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '3',
+		'runners' => '1, 3, 4, 5, 6, 7, 8, 9, 10',
+		'win inter' => '3',
+		'count sets' => 12,
+		'allValues' => '1, 3, 4, 5, 6, 7, 8, 9',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '9',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '9',
+		'count sets' => 15,
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '1',
+		'runners' => '1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '1',
+		'count sets' => 11,
+		'allValues' => '1, 2, 3, 5, 6, 7, 8, 9, 10, 11',
+	],
+];
+//total win: -120
+//total place: 0
+//total qin: 0
+//total trio: 0
+//total: 0
