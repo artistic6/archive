76a9e83a08c2d1ef797d4cc136670b6296f027ea
artistic3 <artisticsoftware3@gmail.com>
2024-06-26 01:31:05 +0000

    ...

--- 0626/bets.php
+++ 0626/bets.php
@@ -78,7 +78,7 @@ return [
 		/**
 		Race 8
 		*/
-		'favorites' => '2, 3, 5, 12',
+		'favorites' => '2, 3, 5, 9, 12',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'win inter' => '',
 		'count sets' => 12,
@@ -88,11 +88,11 @@ return [
 		/**
 		Race 9
 		*/
-		'favorites' => '2, 9',
-		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '2, 9',
-		'count sets' => 15,
-		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'favorites' => '2, 9, 10',
+		'runners' => '1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '',
+		'count sets' => 11,
+		'allValues' => '1, 2, 3, 5, 6, 7, 8, 9, 10, 11',
 	],
 ];
 //total win: -180
