d41cf28132223ec969dcc3144b3cf04ceb10807c
artistic3 <artisticsoftware3@gmail.com>
2024-06-30 04:24:49 +0000

    ...

--- 0701/bets2.php
+++ 0701/bets2.php
@@ -10,7 +10,6 @@ return [
 		'win inter' => '',
 		'count sets' => 21,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
-		'allValues sorted by place odds' => '7, 2, 6, 1, 10, 3, 8, 9, 4, 5',
 	],
 	'2' => [
 		/**
@@ -21,7 +20,6 @@ return [
 		'win inter' => '10',
 		'count sets' => 10,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
-		'allValues sorted by place odds' => '9, 5, 7, 4, 8, 3, 2, 6, 1',
 	],
 	'3' => [
 		/**
@@ -32,7 +30,6 @@ return [
 		'win inter' => '2',
 		'count sets' => 4,
 		'allValues' => '1, 2, 3, 5, 6, 7',
-		'allValues sorted by place odds' => '2, 6, 3, 1, 5, 7',
 	],
 	'4' => [
 		/**
@@ -43,7 +40,6 @@ return [
 		'win inter' => '',
 		'count sets' => 15,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12',
-		'allValues sorted by place odds' => '3, 12, 7, 2, 10, 6, 4, 1, 11, 8, 5',
 	],
 	'5' => [
 		/**
@@ -54,7 +50,6 @@ return [
 		'win inter' => '4',
 		'count sets' => 2,
 		'allValues' => '1, 3, 4, 5, 10, 11',
-		'allValues sorted by place odds' => '4, 10, 11, 5, 1, 3',
 	],
 	'6' => [
 		/**
@@ -65,7 +60,6 @@ return [
 		'win inter' => '1, 4',
 		'count sets' => 5,
 		'allValues' => '1, 2, 3, 5, 6, 7, 8, 9',
-		'allValues sorted by place odds' => '7, 1, 8, 5, 2, 6, 3, 9',
 		'win($30)' => '1, 4',
 	],
 	'7' => [
@@ -77,7 +71,6 @@ return [
 		'win inter' => '',
 		'count sets' => 22,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
-		'allValues sorted by place odds' => '5, 1, 9, 6, 7, 3, 2, 8, 4',
 	],
 	'8' => [
 		/**
@@ -88,7 +81,6 @@ return [
 		'win inter' => '',
 		'count sets' => 15,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8',
-		'allValues sorted by place odds' => '7, 8, 2, 6, 1, 5, 4, 3',
 	],
 	'9' => [
 		/**
@@ -99,7 +91,6 @@ return [
 		'win inter' => '6',
 		'count sets' => 16,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
-		'allValues sorted by place odds' => '6, 3, 2, 9, 1, 11, 4, 8, 7, 10, 5',
 	],
 	'10' => [
 		/**
@@ -110,7 +101,6 @@ return [
 		'win inter' => '4',
 		'count sets' => 0,
 		'allValues' => '',
-		'allValues sorted by place odds' => '',
 		'win($30)' => '',
 	],
 	'11' => [
@@ -122,7 +112,6 @@ return [
 		'win inter' => '',
 		'count sets' => 0,
 		'allValues' => '',
-		'allValues sorted by place odds' => '',
 		'win($30)' => '',
 	],
 ];
