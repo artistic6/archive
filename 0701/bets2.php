e4f907bf066181a284bf2a9664f661d6cf24dd92
artistic3 <artisticsoftware3@gmail.com>
2024-06-30 04:31:06 +0000

    ...

--- 0701/bets2.php
+++ 0701/bets2.php
@@ -76,9 +76,9 @@ return [
 		/**
 		Race 8
 		*/
-		'favorites' => '7',
+		'favorites' => '7, 8',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
-		'win inter' => '',
+		'win inter' => '8',
 		'count sets' => 15,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8',
 	],
