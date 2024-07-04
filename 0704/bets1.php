47059a66e78d4d6a924eb7f4a0f655113ec3287d
artistic3 <artisticsoftware3@gmail.com>
2024-07-04 09:56:57 +0000

    ...

--- 0704/bets1.php
+++ 0704/bets1.php
@@ -71,9 +71,9 @@ return [
 		/**
 		Race 7
 		*/
-		'favorites' => '1, 4, 6',
+		'favorites' => '1, 3, 4, 6',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '1',
+		'win inter' => '1, 3',
 		'count sets' => 16,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7',
 		'place(count 2, $30)' => '4, 6',
--- 0704/bets2.php
+++ 0704/bets2.php
@@ -72,9 +72,9 @@ return [
 		/**
 		Race 7
 		*/
-		'favorites' => '1, 4, 6',
+		'favorites' => '1, 3, 4, 6',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
-		'win inter' => '1',
+		'win inter' => '1, 3',
 		'count sets' => 22,
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
 		'place(count 2, $30)' => '4, 6',
