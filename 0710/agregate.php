b3dfc94f1647f8f5ac95bd8305030adb042ccce8
artistic5 <artisticsoftware5@gmail.com>
2024-07-09 06:39:01 +0000

    ...

--- 0710/agregate.php
+++ 0710/agregate.php
@@ -5,13 +5,13 @@ return [
 		/**
 		Race 1
 		*/
-		'win(allValues, $30)' => '1, 4, 5, 6, 7, 8',
+		'win(allValues, $30)' => '1, 2, 3, 5, 7, 8',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'win(allValues, $30)' => '1, 2, 3, 4, 5, 6',
+		'win(allValues, $30)' => '2, 3, 4, 5, 6, 8',
 	],
 	'7' => [
 		/**
@@ -32,8 +32,8 @@ return [
 		Race 9
 		*/
 		'place(end-favorites, $60)' => '8',
+		'win(allValues, $30)' => '2, 3, 4, 8, 9, 10',
 		'win(wp, 30)' => '2, 3, 8',
 		'place(end-wp, $60)' => '8',
-		'win(allValues, $30)' => '2, 3, 4, 8, 9, 10',
 	],
 ];
