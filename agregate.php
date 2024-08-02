daa1b8abe6a31323e4eff8d90b7f51af2fccded4
artistic software <artisticsoftware41@gmail.com>
2024-08-02 03:41:53 +0000

    ...

--- agregate.php
+++ agregate.php
@@ -6,6 +6,8 @@ $raceDate = trim($argv[1]);
 $currentDir = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
 $outFile = $currentDir . DIRECTORY_SEPARATOR . "agregate.php";
 
+if(file_exists($outFile)) $oldData = include($outFile);
+
 $mainBetsFile = $currentDir . DIRECTORY_SEPARATOR . "bets.php";
 $mainData = include($mainBetsFile);
 $numberOfRaces = count($mainData);
@@ -13,9 +15,6 @@ $outtext = "<?php\n\n";
 $outtext .= "return [\n";
 
 $bets = [];
-$placesFav = [];
-$placesWP = [];
-$places = [];
 for($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber ++) $bets[$raceNumber] = ['favorites' => '(F) ' . $mainData[$raceNumber]['favorites']];
 $dir = new DirectoryIterator($currentDir); 
 foreach ($dir as $fileinfo) {
@@ -23,19 +22,25 @@ foreach ($dir as $fileinfo) {
         $fullFilePath = $currentDir . DIRECTORY_SEPARATOR . $fileinfo->getFilename();
         $fileContents = include($fullFilePath);
         foreach($fileContents as $raceNumber => $data){
-            if(!isset($placesFav[$raceNumber])) $placesFav[$raceNumber] = [];
-            if(!isset($placesWP[$raceNumber])) $placesWP[$raceNumber] = [];
+            $placesFav = [];
+            $placesWP = [];
+            $places = [];
+            if(isset($oldData[$raceNumber]['places'])) $oldPlaces = explode(", ", $oldData[$raceNumber]['places']);
+            else $oldPlaces = [];
             if(isset($data['bets'])) {
                 foreach($data['bets'] as $key => $value){
                     if(!in_array($value, $bets[$raceNumber])) {
                         $bets[$raceNumber][$key] = $value;
                     }
-                    if(strpos($key, "place(end-wp") === 0 && !in_array($value, $placesWP[$raceNumber])) $placesWP[$raceNumber][] = $value;
-                    if(strpos($key, "place(end-fa") === 0 && !in_array($value, $placesFav[$raceNumber])) $placesFav[$raceNumber][] = $value;
+                    if(strpos($key, "place(end-wp") === 0 && !in_array($value, $placesWP)) $placesWP[] = $value;
+                    if(strpos($key, "place(end-fa") === 0 && !in_array($value, $placesFav)) $placesFav[] = $value;
                 }
             }
-            $places[$raceNumber] = array_intersect($placesFav[$raceNumber], $placesWP[$raceNumber]);
-            if(!empty($places[$raceNumber])) $bets[$raceNumber]['sure place'] = implode(", ", $places[$raceNumber]);
+            $places = array_intersect($placesFav, $placesWP);
+            $oldPlaces = array_values(array_unique(array_merge($oldPlaces, $placesFav, $placesWP)));
+            if(!empty($places)) $bets[$raceNumber]['sure place'] = implode(", ", $places);
+            sort($oldPlaces);
+            if(!empty($oldPlaces)) $bets[$raceNumber]['places'] = implode(", ", $oldPlaces);
         }
     }
 }
