fb3efbd2a45f9ddb636e4ca56ef56bd06554f6c4
artistic software <artisticsoftware41@gmail.com>
2024-08-10 07:33:29 +0000

    ...

--- aurl.php
+++ aurl.php
@@ -3,7 +3,6 @@ $endpoint = "https://info.cld.hkjc.com/graphql/base/";
 $date = "2024-08-11";
 $venueCode = "S2";
 $raceNo = 1;
-// $queryJSON = 'query racing($date: String, $venueCode: String, $oddsTypes: [OddsType], $raceNo: Int) {\n  raceMeetings(date: $date, venueCode: $venueCode) {\n    pmPools(oddsTypes: $oddsTypes, raceNo: $raceNo) {\n      id\n      status\n      sellStatus\n      oddsType\n      lastUpdateTime\n      guarantee\n      minTicketCost\n      name_en\n      name_ch\n      leg {\n        number\n        races\n      }\n      cWinSelections {\n        composite\n        name_ch\n        name_en\n        starters\n      }\n      oddsNodes {\n        combString\n        oddsValue\n        hotFavourite\n        oddsDropValue\n        bankerOdds {\n          combString\n          oddsValue\n        }\n      }\n    }\n  }\n}';
 $qryParams = [];
 $qryParams["operationName"] = "racing";
 $qryParams["variables"] = [
@@ -53,6 +52,7 @@ $headers[] = 'Content-Type: application/json';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $endpoint);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+curl_setopt($ch, CURLOPT_ENCODING , '');
 curl_setopt($ch, CURLOPT_POSTFIELDS, $qry);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
