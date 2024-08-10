b5227ad8d1d1c92db025245217e9ae94aaa0d299
artistic software <artisticsoftware41@gmail.com>
2024-08-10 04:28:20 +0000

    ...

---
+++ 0811/agregate.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 2',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 1',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 3',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 5',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 3',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 3',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 5',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 6',
+	],
+];
