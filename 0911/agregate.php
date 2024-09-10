bfab3c681f79cafaaaea0614d8a11c30d2ccd02c
artistic software <artisticsoftware41@gmail.com>
2024-09-10 04:35:39 +0000

    ...

---
+++ 0911/agregate.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 3',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 3',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 1',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 9',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 1',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 5',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 10',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 3',
+	],
+];
---
+++ 0911/bets240.php
@@ -0,0 +1,104 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
+		'win inter' => '2, 3, 4',
+		'allValues' => '1, 2, 4, 5, 6, 7, 8, 9',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8',
+		'win inter' => '2, 5, 6, 7, 8',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '1',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8',
+		'win inter' => '1, 2, 3, 4, 7',
+		'allValues' => '1, 2, 3, 5, 6, 7',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '9',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'win inter' => '2, 6',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '1',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '1, 2, 4, 5, 7',
+		'allValues' => '1, 2, 3, 4, 5',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '1, 2, 3, 5, 6',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '10',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'win inter' => '2, 3, 5, 8, 10',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
+		'win inter' => '3, 4, 5, 6, 7, 8, 12',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8',
+		'bets' => [
+		],
+		'total bets' => 0,
+	],
+];
+//total major place favorites: 0
+//total major place wp: 0
+//total minor place: 0
+//total: 0
