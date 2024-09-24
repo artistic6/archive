f1571e135a0546da20fa26f92e3c9f420fcfc434
artistic3 <artisticsoftware3@gmail.com>
2024-09-24 04:34:04 +0000

    ...

--- 0925/bets834.php
+++ 0925/bets834.php
@@ -5,7 +5,7 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '1',
+		'favorites' => '1, 10',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
 		'win inter' => '1, 3, 4, 6, 7, 8',
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
