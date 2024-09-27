28ee9ab7e9d4f011b856ddaa7cbfcc234abbb47f
artistic3 <artisticsoftware3@gmail.com>
2024-09-27 04:22:20 +0000

    ...

---
+++ 0928/agregate.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 7',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 5',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 3',
+		'place(end-favorites 1, $100)' => '7',
+		'places' => '7',
+		'sures' => '7',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 3',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 2',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 12',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 1',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 2',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 6',
+		'place(end-wp 1, $100)' => '8',
+		'places' => '8',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '(F) 1',
+	],
+];
