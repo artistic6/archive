b495e7d535abae65ab1aa8f1cbfb3c9000df22cb
artistic5 <artisticsoftware5@gmail.com>
2024-10-04 08:04:43 +0000

    ...

--- bets.php
+++ bets.php
@@ -171,10 +171,20 @@ for ($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber++) {
     $racetext .= "\t\t'diff1' => '" . implode(", ", $diff1) . "',\n";
     $diff2 = array_diff($runners, $suggestions["trio"]);
     $racetext .= "\t\t'diff2' => '" . implode(", ", $diff2) . "',\n";
-    if(!empty($allValues) && !empty($suggestions["trio"]) && (count($diff1) <= 2 || count($diff2) <= 2))
-        $racetext .= "\t\t'win bet' => '" . implode(", ", array_intersect($allValues, $suggestions["qin"])) . "',\n"; 
+    if(!empty($allValues) && !empty($suggestions["trio"])){
+        $experimental = array_intersect($allValues, $suggestions["qin"]);
+        $racetext .= "\t\t'win bet' => '" . implode(", ", $experimental) . "',\n";
+        $favInter = array_intersect($experimental, $favorites);
+        $racetext .= "\t\t'inter fav' => '" . implode(", ", $favInter) . "',\n";
+        if(Count($favInter) >= 4 && count($favorites) < 6) {
+            $racetext .= "\t\t'sure win ??' => '" . implode(", ", $favorites) . "',\n";
+            if(isset($officialWin)){
+                $racetext .= "\t\t'official win' => '" . implode(", ", $officialWin) . "',\n"; 
+            }
+        }
+    } 
     $racetext .= "\t\t'bets' => [\n";
-    if(count($favorites) >= 3 && in_array(count($winInter), [3, 4, 5])){
+    if(!empty($diff1) && !empty($diff2) && count($favorites) >= 3 && in_array(count($winInter), [3, 4, 5])){
         $racetext .= "\t\t\t'place(end-favorites $revision, $" . $unitBet . ")' => '" .  end($favorites)  . "',\n"; 
         $totalBets[$raceNumber] += $unitBet;
         $totalMajorPlaceF -= $unitBet;
@@ -195,7 +205,7 @@ for ($raceNumber = 1; $raceNumber <= $numberOfRaces; $raceNumber++) {
         }
     }
     $wp = array_intersect($allValues, $favorites);
-    if(count($wp) === 3){
+    if(!empty($diff1) && !empty($diff2) && count($wp) === 3){
         $racetext .= "\t\t\t'place(end-wp $revision, $" . $unitBet . ")' => '" . end($wp) . "',\n"; 
         $totalBets[$raceNumber] += $unitBet;
         $totalMajorPlaceW -= $unitBet;
