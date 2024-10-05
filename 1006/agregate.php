c935e0abd8e77688bcc9e66f1407b8756569f96c
artistic5 <artisticsoftware5@gmail.com>
2024-10-05 04:18:41 +0000

    ...

---
+++ 1006/agregate.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 1',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 2',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 8',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 1',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 5',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 5',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 7',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 5',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 6',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '(F) 1',
+	],
+];
