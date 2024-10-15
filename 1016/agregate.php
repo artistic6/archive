d58c823e7a72b1d5126dd60dda5ea32014c995f4
artistic5 <artisticsoftware5@gmail.com>
2024-10-15 04:18:15 +0000

    ...

---
+++ 1016/agregate.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 8',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 1, 5',
+		'win(union 13)' => '1, 5, 6, 9',
+		'win(union 43)' => '1, 4, 5, 9',
+		'win(union 693)' => '1, 5, 9',
+		'win(union 1442)' => '1, 4, 5, 6',
+		'win(union 2565)' => '1, 5, 6',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 11',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 11',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 2, 5',
+		'win(union 990)' => '2, 4, 5, 9',
+		'win(union 2027)' => '1, 2, 5, 9',
+		'win(union 2028)' => '1, 2, 4, 5',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 6',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 2, 4',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 5, 8',
+	],
+];
