3a5ddfdc698db031adf1066dcfbea655f00785b4
artistic6 <artisticsoftware6@gmail.com>
2024-10-26 05:30:28 +0000

    ...

--- 1027S1/bets757.php
+++ 1027S1/bets757.php
@@ -5,7 +5,7 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '6, 12',
+		'favorites' => '6, 8, 12',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15',
 		'suggestions' => [
 			'win' => '',
@@ -13,19 +13,17 @@ return [
 			'trio' => '',//count trio: 0
 			'inter' => '',
 		],
-		'win inter' => '1, 7',
+		'win inter' => '',
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
 		'bets' => [
-			'win(union 757)' => '1, 6, 7, 12',
-			'win(experimental 757)' => '1, 6, 7, 12',
 		],
-		'total bets' => 400,
-		'wp' => '6',//count wp: 1
+		'total bets' => 0,
+		'wp' => '6, 8',//count wp: 2
 	],
 ];
 //total place end favorites: 0
 //total place end wp: 0
 //total place wp: 0
 //total sure place: 0
-//total win: -400
+//total win: 0
 //total: 0
